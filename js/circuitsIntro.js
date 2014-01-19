bb980e06f407941ff76bbe24f04a299ea92aba1a
Akshayab <akshaybudhkar@gmail.com>
2014-01-19 01:46:22 -0500

    Includes 1st lesson in circuit Mania

---
+++ js/circuitsIntro.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('#voltageName').css("left", width_grid + 10 + "px");
+  $('#voltageName').css("top", 2*height_grid + 10 + "px");
+  $('#currentName').css("left", width_grid + 10 + "px");
+  $('#currentName').css("top", 3*height_grid + 10 + "px");
+  $('#resistanceName').css("left", width_grid + 10 + "px");
+  $('#resistanceName').css("top", 4*height_grid + 10 + "px");
+  $('#voltageAnswer').css("top", 2*height_grid + 10 + "px");
+  $('#voltageAnswer').css("left", 4*height_grid + 10 + "px");
+  $('#currentAnswer').css("left", 4*width_grid + 10 + "px");
+  $('#currentAnswer').css("top", 4*height_grid + 10 + "px");
+  $('#resistanceAnswer').css("left", 4*height_grid + 10 + "px");
+  $('#resistanceAnswer').css("top", 4*height_grid + 10 + "px");
+});
