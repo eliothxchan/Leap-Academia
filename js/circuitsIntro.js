ec1dcb3c8ab963feeae4ed0f021ff31c4e7b2e9e
Akshayab <akshaybudhkar@gmail.com>
2014-01-19 08:02:09 -0500

    Revert "Changing functionality of circuits parts page"
    This reverts commit 31b6cfcdc118f6103ad7ded0bd20bbe54224baa7.

--- js/circuitsIntro.js
+++ js/circuitsIntro.js
@@ -19,10 +19,7 @@ $(document).ready(function(){
   
   $('#voltage').css("top", height_grid + 10 + "px ");
   $('#resistance').css("top", 2*height_grid + 10 + "px ");
-  $('#current').css("top", 3*height_grid + 10 + "px "); 
-  $('#voltage').css("left",  10 + "px ");
-  $('#resistance').css("left", 10 + "px ");
-  $('#current').css("left",  10 + "px ");
+  $('#current').css("top", 3*height_grid + 10 + "px ");
   
   $('#description, #description2').css("top", height_grid + 10 + "px ");
   $('#description, #description2').css("left", 8*height_grid + 10 + "px ");
